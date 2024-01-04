4eb32bbb7b30361d4f93baa9cb2bd872cdc29193
Callum Laing <callum.laing91@hotmail.co.uk>
2024-01-04 22:35:09 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import "./App.css";
+import Title from "./components/Title";
+
+function App() {
+  return (
+    <div>
+      <Title />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Title.jsx
@@ -0,0 +1,11 @@
+import "./Title.css";
+
+const Title = () => {
+  return (
+    <div className="titleContainer">
+      <h1>React Playground</h1>
+    </div>
+  );
+};
+
+export default Title;
