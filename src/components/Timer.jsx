5ac88c8b109413fc515ead0c620423cfa83ebd59
Callum Laing <callum.laing91@hotmail.co.uk>
2024-01-10 23:06:12 +0000

    added countdown function to Timer component

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -3,26 +3,40 @@ import "./Timer.css";
 
 const Timer = () => {
   let [count, setCount] = useState(30);
+  const [isRunning, setIsRunning] = useState(false);
 
   const countdown = () => {
     if (count > 0) {
       setCount(count - 1);
     } else {
       console.log("count is 0");
+      setIsRunning(false);
     }
   };
 
+  const toggleTimer = () => {
+    setIsRunning((prevIsRunning) => !prevIsRunning);
+  };
+
   useEffect(() => {
-    const intervalId = setInterval(countdown, 1000);
+    let intervalId;
+
+    if (isRunning) {
+      intervalId = setInterval(countdown, 1000);
+    } else {
+      clearInterval(intervalId);
+    }
 
     return () => {
       clearInterval(intervalId);
     };
-  }, [count]);
+  }, [count, isRunning]);
 
   return (
     <div>
-      <button className="timerBtn">{count}</button>
+      <button className="timerBtn" onClick={toggleTimer} disabled={isRunning}>
+        {count}
+      </button>
     </div>
   );
 };
