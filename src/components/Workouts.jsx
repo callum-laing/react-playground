61a554970ff216f68464b0b2ec0e56d7a55c242f
Callum Laing <callum.laing91@hotmail.co.uk>
2024-01-15 21:46:03 +0000

    added more to Workouts.jsx

--- src/components/Workouts.jsx
+++ src/components/Workouts.jsx
@@ -1,14 +1,47 @@
 import "./Workouts.css";
+import React, { useState, useEffect } from "react";
 
 const Workout = () => {
+  const [workoutId, setWorkoutId] = useState(1);
+  const [workoutTxt, setWorkoutTxt] = useState("");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`/workouts/${workoutId}.txt`);
+        const workoutId = await response.text();
+        setWorkoutTxt(workoutTxt);
+      } catch (error) {
+        console.error("Error fetching workout data: ", error);
+      }
+    };
+
+    fetchData();
+  }, [workoutId]);
+
+  const handlePrev = () => {
+    if (workoutId > 1) {
+      setWorkoutId(workoutId - 1);
+    }
+  };
+
+  const handleNext = () => {
+    if (workoutId < 4) {
+      setWorkoutId(workoutId + 1);
+    }
+  };
+
   return (
     <div className="workout">
-      <button className="workBtn">Prev</button>
-      <button className="workBtn">Next</button>
+      <button onClick={handlePrev} className="workBtn">
+        Prev
+      </button>
+      <button onClick={handleNext} className="workBtn">
+        Next
+      </button>
       <div className="workText">
-        <h2>Workout #1</h2>
-        <p>Burpees 10 reps</p>
-        <p>Push Ups 10 reps</p>
+        <h2>Workout #{workoutId}</h2>
+        <p>{workoutTxt}</p>
       </div>
     </div>
   );
